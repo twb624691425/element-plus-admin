a4b716699a99d182b3e308c222cbee7017327755
tangweibin <624691425@qq.com>
2023-03-14 17:22:28 +0800

    fit(*): 限制只能使用yarn

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <router-view />
+  <el-config-provider :size="size" :z-index="zIndex">
+    <router-view />
+  </el-config-provider>
 </template>
 
+<script setup>
+import { ElConfigProvider } from 'element-plus'
+import { ref } from 'vue'
+const zIndex = ref(2000)
+const size = ref('small')
+</script>
 
